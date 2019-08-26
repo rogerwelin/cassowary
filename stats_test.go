fcddfa2c397dead3fe6708426c90270d1549439c
rogerwelin <roger.welin@icloud.com>
2019-08-26 21:06:15 +0200

    stddev test cases

--- stats_test.go
+++ stats_test.go
@@ -8,12 +8,13 @@ var testNums = []struct {
 	in             []float64
 	expectedMean   float64
 	expectedMedian float64
+	expectedStdDev float64
 }{
-	{[]float64{-10, 0, 10, 20, 30}, 10, 10},
-	{[]float64{8, 9, 10, 11, 12}, 10, 10},
-	{[]float64{40, 10, 20, 30, 1}, 20.20, 20},
-	{[]float64{3, 2, 2}, 2.3333333333333335, 2},
-	{[]float64{3, 2, 1, 9}, 3.75, 2.5},
+	{[]float64{-10, 0, 10, 20, 30}, 10, 10, 14.142135623730951},
+	{[]float64{8, 9, 10, 11, 12}, 10, 10, 1.4142135623730951},
+	{[]float64{40, 10, 20, 30, 1}, 20.20, 20, 13.862178760930766},
+	{[]float64{3, 2, 2}, 2.3333333333333335, 2, 0.4714045207910317},
+	{[]float64{3, 2, 1, 9}, 3.75, 2.5, 3.112474899497183},
 }
 
 func TestCalcMean(t *testing.T) {
@@ -30,6 +31,15 @@ func TestCalcMedian(t *testing.T) {
 		actual := calcMedian(tt.in)
 		if actual != tt.expectedMedian {
 			t.Errorf("calcMean(%f): expected %f, actual %f", tt.in, tt.expectedMedian, actual)
+		}
+	}
+}
+
+func TestCalcStdDev(t *testing.T) {
+	for _, tt := range testNums {
+		actual := calcStdDev(tt.in)
+		if actual != tt.expectedStdDev {
+			t.Errorf("calcMean(%f): expected %f, actual %f", tt.in, tt.expectedStdDev, actual)
 		}
 	}
 }
