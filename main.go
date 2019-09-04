34f9cf5d5d0c911f00960f78317bddfd4b7a19b1
rogerwelin <roger.welin@icloud.com>
2019-09-04 19:00:20 +0200

    first sketch of cli logic

--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"os"
 
 	"github.com/urfave/cli"
 )
 
+var (
+	errBaseURLRequired  = errors.New("-u, --base-url argument is required")
+	errFileRequired     = errors.New("-f, --file argument is required required")
+	errURLRequired      = errors.New("-u, --url argument is required")
+	errRequestsRequired = errors.New("-r, --requests argument is required")
+
+	baseURL          string
+	filePath         string
+	concurrencyLevel int
+	noOfRequests     int
+)
+
 type cassowary struct {
-	inputFile        os.File
+	inputFile        string
 	baseURL          string
 	concurrencyLevel int
 	requests         int
@@ -24,4 +38,68 @@ func validateRunFile(c *cli.Context) {
 func main() {
 	app := cli.NewApp()
 	app.Name = "cassowary"
+	app.HelpName = "cassowary"
+	app.UsageText = "cassowary [command] [command options] [arguments...]"
+	app.EnableBashCompletion = true
+	app.Usage = ""
+	app.Commands = []cli.Command{
+		{
+			Name:  "run-file",
+			Usage: "start load test",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:        "u, base-url",
+					Usage:       "the base url (absoluteURI) to be used",
+					Destination: &baseURL,
+				},
+				cli.IntFlag{
+					Name:        "c, concurrency",
+					Usage:       "number of concurrent users. defaults to 1",
+					Destination: &concurrencyLevel,
+				},
+				cli.StringFlag{
+					Name:        "f, file",
+					Usage:       "specify `FILE` path, local or www, containing the url suffixes (absolute paths)",
+					Destination: &filePath,
+				},
+			},
+			Action: validateRunFile,
+		},
+		{
+			Name:  "run",
+			Usage: "start load-test",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:        "u, url",
+					Usage:       "the url (absoluteURI) to be used",
+					Destination: &baseURL,
+				},
+				cli.IntFlag{
+					Name:        "c, concurrency",
+					Usage:       "number of concurrent users. defaults to 1",
+					Destination: &concurrencyLevel,
+				},
+				cli.IntFlag{
+					Name:        "r, requests",
+					Usage:       "number of requests",
+					Destination: &noOfRequests,
+				},
+			},
+			Action: validateRun,
+		},
+	}
+
+	app.CommandNotFound = func(c *cli.Context, command string) {
+		err := cli.ShowAppHelp(c)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(0)
+		}
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(0)
+	}
 }
