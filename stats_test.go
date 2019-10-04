837a5ea82cb2d58e4efe22e3048947aaf5ec13ac
rogerwelin <roger.welin@icloud.com>
2019-10-04 12:35:44 +0200

    add more stats test

--- stats_test.go
+++ stats_test.go
@@ -5,16 +5,17 @@ import (
 )
 
 var testNums = []struct {
-	in             []float64
-	expectedMean   float64
-	expectedMedian float64
-	expectedStdDev float64
+	in               []float64
+	expectedMean     float64
+	expectedMedian   float64
+	expectedVariance float64
+	expectedStdDev   float64
 }{
-	{[]float64{-10, 0, 10, 20, 30}, 10, 10, 14.142135623730951},
-	{[]float64{8, 9, 10, 11, 12}, 10, 10, 1.4142135623730951},
-	{[]float64{40, 10, 20, 30, 1}, 20.20, 20, 13.862178760930766},
-	{[]float64{3, 2, 2}, 2.3333333333333335, 2, 0.4714045207910317},
-	{[]float64{3, 2, 1, 9}, 3.75, 2.5, 3.112474899497183},
+	{[]float64{-10, 0, 10, 20, 30}, 10, 10, 200, 14.142135623730951},
+	{[]float64{8, 9, 10, 11, 12}, 10, 10, 2, 1.4142135623730951},
+	{[]float64{40, 10, 20, 30, 1}, 20.20, 20, 192.16, 13.862178760930766},
+	{[]float64{3, 2, 2}, 2.3333333333333335, 2, 0.22222222222222224, 0.4714045207910317},
+	{[]float64{3, 2, 1, 9}, 3.75, 2.5, 9.6875, 3.112474899497183},
 }
 
 var testStatusCodes = []struct {
@@ -38,6 +39,15 @@ func TestCalcMedian(t *testing.T) {
 		actual := calcMedian(tt.in)
 		if actual != tt.expectedMedian {
 			t.Errorf("test: %d, calcMedian(%f): expected %f, actual %f", i+1, tt.in, tt.expectedMedian, actual)
+		}
+	}
+}
+
+func TestCalcVariance(t *testing.T) {
+	for i, tt := range testNums {
+		actual := calcVarience(tt.in)
+		if actual != tt.expectedVariance {
+			t.Errorf("test: %d, calcVariance(%f): expected %f, actual %f", i+1, tt.in, tt.expectedVariance, actual)
 		}
 	}
 }
