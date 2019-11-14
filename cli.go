40c9d533c65b42e57d639a783e6ed191b4706c32
rogerwelin <roger.welin@icloud.com>
2019-11-14 18:39:50 +0100

    major refactoring of the cli

--- cli.go
+++ cli.go
@@ -11,18 +11,8 @@ import (
 )
 
 var (
-	errBaseURLRequired  = errors.New("-u, --base-url argument is required")
-	errFileRequired     = errors.New("-f, --file argument is required required")
-	errURLRequired      = errors.New("-u, --url argument is required")
-	errRequestsRequired = errors.New("-n, --requests argument is required")
-
-	baseURL          string
-	filePath         string
-	concurrencyLevel int
-	noOfRequests     int
-	promExport       bool
-	promGwURL        string
-	httpHeader       string
+	errConcurrencyLevel = errors.New("Error: Concurrency level cannot be set to: 0")
+	errRequestNo        = errors.New("Error: No. of request cannot be set to: 0")
 )
 
 type cassowary struct {
@@ -39,63 +29,62 @@ type cassowary struct {
 }
 
 func validateRun(c *cli.Context) error {
-	if baseURL == "" {
-		return errURLRequired
+
+	prometheusEnabled := false
+
+	if c.Int("concurrency") == 0 {
+		return errConcurrencyLevel
 	}
-	if noOfRequests == 0 {
-		return errRequestsRequired
+
+	if c.Int("requests") == 0 {
+		return errRequestNo
 	}
-	if concurrencyLevel == 0 {
-		concurrencyLevel = 1
-	}
-	if promGwURL == "" {
-		promExport = false
-	} else {
-		promExport = true
+
+	if c.String("prompushgwurl") != "" {
+		prometheusEnabled = true
 	}
 
 	cass := &cassowary{
 		fileMode:         false,
-		baseURL:          baseURL,
-		concurrencyLevel: concurrencyLevel,
-		requests:         noOfRequests,
-		promExport:       promExport,
-		promURL:          promGwURL,
+		baseURL:          c.String("url"),
+		concurrencyLevel: c.Int("concurrency"),
+		requests:         c.Int("requests"),
+		promExport:       prometheusEnabled,
+		promURL:          c.String("prompushgwurl"),
 	}
 
+	//fmt.Printf("%+v\n", cass)
 	cass.coordinate()
-
 	return nil
 }
 
 func validateRunFile(c *cli.Context) error {
-	if baseURL == "" {
-		return errBaseURLRequired
+
+	prometheusEnabled := false
+
+	if c.Int("concurrency") == 0 {
+		return errConcurrencyLevel
 	}
-	if filePath == "" {
-		return errFileRequired
+
+	if c.Int("requests") == 0 {
+		return errRequestNo
 	}
-	if concurrencyLevel == 0 {
-		concurrencyLevel = 1
-	}
-	if promGwURL == "" {
-		promExport = false
-	} else {
-		promExport = true
+
+	if c.String("prompushgwurl") != "" {
+		prometheusEnabled = true
 	}
 
 	cass := &cassowary{
 		fileMode:         true,
-		inputFile:        filePath,
-		baseURL:          baseURL,
-		concurrencyLevel: concurrencyLevel,
-		requests:         noOfRequests,
-		promExport:       promExport,
-		promURL:          promGwURL,
+		inputFile:        c.String("file"),
+		baseURL:          c.String("base-url"),
+		concurrencyLevel: c.Int("concurrency"),
+		requests:         c.Int("requests"),
+		promExport:       prometheusEnabled,
+		promURL:          c.String("prompushgwurl"),
 	}
 
 	cass.coordinate()
-
 	return nil
 }
 
@@ -112,29 +101,27 @@ func runCLI(args []string) {
 			Usage: "start load test",
 			Flags: []cli.Flag{
 				cli.StringFlag{
-					Name:        "u, base-url",
-					Usage:       "the base url (absoluteURI) to be used",
-					Destination: &baseURL,
+					Name:     "u, base-url",
+					Usage:    "the base url (absoluteURI) to be used",
+					Required: true,
 				},
 				cli.IntFlag{
-					Name:        "c, concurrency",
-					Usage:       "number of concurrent users. defaults to 1",
-					Destination: &concurrencyLevel,
+					Name:     "c, concurrency",
+					Usage:    "number of concurrent users",
+					Required: true,
 				},
 				cli.StringFlag{
-					Name:        "f, file",
-					Usage:       "specify `FILE` path, local or www, containing the url suffixes",
-					Destination: &filePath,
+					Name:     "f, file",
+					Usage:    "specify `FILE` path, local or www, containing the url suffixes",
+					Required: true,
 				},
 				cli.StringFlag{
-					Name:        "p, prompushgwurl",
-					Usage:       "specify prometheus push gateway url to send metrics (optional)",
-					Destination: &promGwURL,
+					Name:  "p, prompushgwurl",
+					Usage: "specify prometheus push gateway url to send metrics (optional)",
 				},
 				cli.StringFlag{
-					Name:        "H, header",
-					Usage:       "Add Arbitrary header line, eg. 'Host: www.example.com'",
-					Destination: &httpHeader,
+					Name:  "H, header",
+					Usage: "Add Arbitrary header line, eg. 'Host: www.example.com'",
 				},
 			},
 			Action: validateRunFile,
@@ -144,41 +131,31 @@ func runCLI(args []string) {
 			Usage: "start load-test",
 			Flags: []cli.Flag{
 				cli.StringFlag{
-					Name:        "u, url",
-					Usage:       "the url (absoluteURI) to be used",
-					Destination: &baseURL,
+					Name:     "u, url",
+					Usage:    "the url (absoluteURI) to be used",
+					Required: true,
 				},
 				cli.IntFlag{
-					Name:        "c, concurrency",
-					Usage:       "number of concurrent users. defaults to 1",
-					Destination: &concurrencyLevel,
+					Name:     "c, concurrency",
+					Usage:    "number of concurrent users",
+					Required: true,
 				},
 				cli.IntFlag{
-					Name:        "n, requests",
-					Usage:       "number of requests to perform",
-					Destination: &noOfRequests,
+					Name:     "n, requests",
+					Usage:    "number of requests to perform",
+					Required: true,
 				},
 				cli.StringFlag{
-					Name:        "p, prompushgwurl",
-					Usage:       "specify prometheus push gateway url to send metrics (optional)",
-					Destination: &promGwURL,
+					Name:  "p, prompushgwurl",
+					Usage: "specify prometheus push gateway url to send metrics (optional)",
 				},
 				cli.StringFlag{
-					Name:        "H, header",
-					Usage:       "Add Arbitrary header line, eg. 'Host: www.example.com'",
-					Destination: &httpHeader,
+					Name:  "H, header",
+					Usage: "Add Arbitrary header line, eg. 'Host: www.example.com'",
 				},
 			},
 			Action: validateRun,
 		},
-	}
-
-	app.CommandNotFound = func(c *cli.Context, command string) {
-		err := cli.ShowAppHelp(c)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(0)
-		}
 	}
 
 	err := app.Run(args)
---
+++ go.mod
@@ -0,0 +1,11 @@
+module cassowary
+
+go 1.13
+
+require (
+	github.com/fatih/color v1.7.0
+	github.com/mattn/go-colorable v0.1.4 // indirect
+	github.com/mattn/go-isatty v0.0.10 // indirect
+	github.com/schollz/progressbar v1.0.0
+	github.com/urfave/cli v1.22.1
+)
