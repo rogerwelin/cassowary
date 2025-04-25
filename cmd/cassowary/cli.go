9728c606acf61939c1fac60e289b9b06e8bed50d
Eddie Niese <eniese@users.noreply.github.com>
2025-04-25 09:17:16 +0200

    Client Certificate Renegotiation support (#58)
    * Added --renegotiation option with values 'never', 'once' and 'freely' for client certificate renegotiation
    
    * Added use of --renegotiation to the x509 example. I can't edit the Chinese documentation unfortunately
    
    * Integrated review comments from rogerwelin

--- cmd/cassowary/cli.go
+++ cmd/cassowary/cli.go
@@ -181,6 +181,19 @@ func validateCLI(c *cli.Context) error {
 		tlsConfig.InsecureSkipVerify = true
 	}
 
+	if c.String("renegotiation") != "" {
+		switch option := c.String("renegotiation"); option {
+		case "never":
+			tlsConfig.Renegotiation = tls.RenegotiateNever
+		case "once":
+			tlsConfig.Renegotiation = tls.RenegotiateOnceAsClient
+		case "freely":
+			tlsConfig.Renegotiation = tls.RenegotiateFreelyAsClient
+		default:
+			return fmt.Errorf("invalid renegotiation option specified: %q", option)
+		}
+	}
+
 	if c.String("ca") != "" {
 		pemCerts, err := os.ReadFile(c.String("ca"))
 		if err != nil {
@@ -338,6 +351,11 @@ func runCLI(args []string) {
 				&cli.BoolFlag{
 					Name:  "insecure",
 					Usage: "use this flag to skip ssl verification",
+				},
+				&cli.StringFlag{
+					Name:  "renegotiation",
+					Usage: "Allow client certificate renegotiation: never, once, freely",
+					Value: "never",
 				},
 				&cli.StringFlag{
 					Name:  "ca",
